9d602edf6f8130d126af416b0ab61a427e0a23e9
Dwight Teepee <Dwight@thinkpad>
2015-08-17 21:32:27 -0400

    Add event lookup

--- src/champs/Event.scala
+++ src/champs/Event.scala
@@ -1,9 +1,87 @@
 package champs
+import PartialFunction._
 
 /**
  * Created by dwight on 08/08/15.
  */
 class Event(eventName:String, eventGender:String, eventClass:String, eventNumber:String) {
 
-  var round  = ""
+  var round  =""
+  var eventCategory:String = Event.getCategory(eventName)
+  var eventType = Event.getEventType(eventName)
+
+  override def toString(): String ="("+ round + "," + eventGender +","+ eventClass +","+ eventName + "," + eventCategory + "," + eventType +")";
 }
+
+object Event {
+
+ def onlyString(v: Any): Option[String] = condOpt(v) { case x: String => x }
+
+  def getCategory(eventName:String): String ={
+     EVENTS.get(eventName) match{
+      case Some(desc) => return onlyString(desc.get("category").get).get
+      case None => return "NA"
+     }
+  }
+
+ def getEventType(eventName:String):String ={
+  EVENTS.get(eventName) match{
+   case Some(desc) => return onlyString(desc.get("type").get).get
+   case None => return "NA"
+  }
+ }
+  def describeEvent(eventName:String): Option[Map[String,Any]] ={
+    return EVENTS.get(eventName)
+  }
+
+  val EVENTS:Map[String,Map[String,Any]] =
+   Map("100 Meter Dash"->Map("category"->"Track","type"->"Sprint","hasWind"-> true  ),
+ "200 Meter Dash" ->Map("category"->"Track","type"->"Sprint","hasWind"-> true ),
+ "400 Meter Dash" ->Map("category"->"Track","type"->"Sprint"),
+ "800 Meter Run" ->Map("category"->"Track","type"->"Distance"),
+ "1500 Meter Run" ->Map("category"->"Track","type"->"Distance"),
+ "2000 Meter Steeplechase"->Map("category"->"Track","type"->"Distance"),
+ "3000 Meter Run"->Map("category"->"Track","type"->"Distance"),
+ "5000 Meter Run"->Map("category"->"Track","type"->"Distance"),
+ "70 Meter Hurdles"->Map("category"->"Track","type"->"Hurdles","hasWind"-> true),
+ "80 Meter Hurdles"->Map("category"->"Track","type"->"Hurdles","hasWind"-> true),
+ "100 Meter Hurdles" ->Map("category"->"Track","type"->"Hurdles","hasWind"-> true ),
+ "110 Meter Hurdles"->Map("category"->"Track","type"->"Hurdles","hasWind"-> true),
+ "400 Meter Hurdles" ->Map("category"->"Track","type"->"Hurdles"),
+ "4 X100 Meter Relay" ->Map("category"->"Track","type"->"Relay"),
+ "4 X400 Meter Relay" ->Map("category"->"Track","type"->"Relay"),
+ "1600 Sprint Medley"->Map("category"->"Track","type"->"Relay"),
+ "High Jump"->Map("category"->"Field","type"->"Jumps"),
+ "Long Jump"->Map("category"->"Field","type"->"Jumps","hasWind"-> true),
+ "Triple Jump"->Map("category"->"Field","type"->"Jumps","hasWind"-> true),
+ "Pole Vault"->Map("category"->"Field","type"->"Jumps"),
+ "Shot Put"->Map("category"->"Field","type"->"Throws"),
+ "Discus Throw"->Map("category"->"Field","type"->"Throws"),
+ "Javelin Throw"->Map("category"->"Field","type"->"Throws"),
+ "Heptathlon"->Map("category"->"Mixed","type"->"Heptathlon"),
+
+ "100 Meter Hurdles Heptathlon"->Map("category"->"Track","type"->"Sprint","hasWind"-> true),
+ "200 Meter Run Heptathlon"->Map("category"->"Track","type"->"Sprint","hasWind"-> true),
+ "800 Meter Run Heptathlon"->Map("category"->"Track","type"->"Distance"),
+ "Javelin Throw Heptathlon"->Map("category"->"Field","type"->"Throws"),
+ "Shot Put Heptathlon"->Map("category"->"Field","type"->"Throws"),
+ "High Jump Heptathlon"->Map("category"->"Field","type"->"Jumps"),
+ "Long Jump Heptathlon"->Map("category"->"Field","type"->"Jumps","hasWind"-> true),
+
+ "Decathlon"->Map("category"->"Mixed","type"->"Decathlon"),
+
+ "100 Meter Run Decathlon"->Map("category"->"Track","type"->"Sprint","hasWind"-> true),
+ "110 Meter Hurdles Decathlon"->Map("category"->"Track","type"->"Hurdles","hasWind"-> true),
+ "400 Meter Dash Decathlon"->Map("category"->"Track","type"->"Sprint"),
+ "1500 Meter Run Decathlon"->Map("category"->"Track","type"->"Distance"),
+ "Javelin Throw Decathlon"->Map("category"->"Field","type"->"Throws"),
+ "Pole Vault Decathlon"->Map("category"->"Field","type"->"Jumps"),
+ "Shot Put Decathlon"->Map("category"->"Field","type"->"Throws"),
+ "Discus Throw Decathlon"->Map("category"->"Field","type"->"Throws"),
+ "High Jump Decathlon"->Map("category"->"Field","type"->"Jumps"),
+ "Long Jump Decathlon"->Map("category"->"Field","type"->"Jumps","hasWind"-> true)
+  )
+
+}
+
+
--- src/champs/parsers/ChampsParser.scala
+++ src/champs/parsers/ChampsParser.scala
@@ -15,10 +15,15 @@ class ChampsParser extends GroupedRegexParser{
   private def EVENT   = regexMatch("""(Event \d{1,2}) *(Boys|Girls) (\d{1,2}-\d{1,2}) (\w+ \D+( \D+)?) (CLASS \d|OPEN)""".r ) ^^
     { case m => (new Event(m.group(4), m.group(2),  title(m.group(6)), m.group(1) )) }
 
-  private def ROUND = regexMatch("""(Preliminaries)""".r) ^^ { case m => (m.group(1))  }
+  private def ROUND = regexMatch("""(Round: |(Flight|Heat)  \d)?\s*?(Preliminaries|Semi-Finals|Finals)""".r) ^^ { case m => (m.group(3))  }
 
   private def PLACING = regexMatch("""^ {0,2}(\d{1,3}|-{2,3}) (\D+,\D+ ?\D+?)? (1[0-9])?([^\d]+)?\s{3,}""".r) ^^
     { case m => (m.group(1), m.group(2).trim(), m.group(3), m.group(4))}
+
+
+  private def PRELIM_PLACING = regexMatch("""^ {0,2}(\d{1,3}|-{2,3}) (\D+,\D+ ?\D+?)? ([1-2][0-9])?([^\d]+)?\s{2,}((\d{1,2}?:?\d{0,2}\.\d{1,2})(m)?)?\s+((\d{1,2}?:?\d{0,2}\.\d{1,2}|ND|NH|DNS|DNF|FOUL)(m)?)(q|Q)?""".r) ^^
+    { case m => (m.group(1), m.group(2).trim(), m.group(3), m.group(4), m.group(5), m.group(8), m.group(11))}
+
 
   private def HEAT = regexMatch("""(((Flight|Heat) {1,3}\d{1,2}) )""".r) ^^ { case m => m.group(1)  }
 
@@ -27,6 +32,11 @@ class ChampsParser extends GroupedRegexParser{
   }
 }
 
+
+
+class PlacingParser extends GroupedRegexParser{
+
+}
 
 object ChampsParser extends ChampsParser{
 
@@ -37,17 +47,39 @@ object ChampsParser extends ChampsParser{
   def main(args: Array[String]) {
 
     val champsSource = Source.fromFile("data/champs2011.txt")
-    var round:Any = None
+    val round:Any = None
+    var heat:Any = None
 
     for (line <- champsSource.getLines()){
       parse(PLACING,line) match{
-        case Success(matched,_) => { println(matched) }
+        case Success(matched,_) => {
+          parsePlacing(events.last, line)
+        }
         case Failure(msg,_) => parseEvent(line)
       }
     }
     //println( events )
   }
 
+  def parsePlacing(event:Event, line:String): Unit ={
+    event.round  match {
+      case "Preliminaries" => parsePrelimPlacing(event, line)
+      case "Semi-Finals" => parsePrelimPlacing(event, line)
+      case "Finals"  => parseFinalsPlacing(event, line)
+      case _ => println("No match for round " )
+    }
+  }
+
+  def parsePrelimPlacing(event:Event, line:String): Unit ={
+    parse(PRELIM_PLACING, line) match {
+      case Success(matched,_) => println(matched)
+      case Failure(msg,_) => println("Prelim No match")
+    }
+  }
+
+  def parseFinalsPlacing(event:Event, line:String): Unit ={
+    //println("")
+  }
 
   def parseEvent(line:String): Unit ={
       parse(EVENT, line) match{
@@ -66,7 +98,7 @@ object ChampsParser extends ChampsParser{
 
     def parseHeat(line:String): Unit = {
       parse(HEAT, line) match{
-        case Success(matched, _) => println(matched)
+        case Success(matched, _) => heat = matched
         case Failure(msg,_) => None
       }
     }
